a37a25dff59bef9668a7d1af0075d24c1e0e9470
Abby <nduta>
2020-05-17 13:40:45 +0300

    edit the board in place

--- Board.py
+++ Board.py
@@ -1,6 +1,20 @@
-def display_board(board):
-    print(board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print(board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print(board[1] + ' | ' + board[2] + ' | ' + board[3])
+user_input = input(" Do you choose X or O? ")
+def show_options():
+    if user_input == 'x'.lower():
+        print(user_input)
+show_options()
 
-display_board ( ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'])
+def display_board(board):
+    for n,z in enumerate(board):
+        board[n] += user_input
+    print((' | ' + board[n] + ' | ')*3, "\n")
+    print((' | ' + board[n] + ' | ') * 3, "\n")
+    print((' | ' + board[n] + ' | ') * 3, "\n")
+    #print(' | ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + ' | ')
+    #print(' | ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + ' | ')
+    #print(' | ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' | ')
+display_board ([' '] *10)
+
+
+#collecting user input
+
