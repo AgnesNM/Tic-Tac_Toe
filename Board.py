842a4b35c62b54e9af7f3b50eeee0019e49ee8fa
Abby <nduta>
2020-05-17 10:17:11 +0300

    create a display board

---
+++ Board.py
@@ -0,0 +1,9 @@
+def display_board(board):
+    print(board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(board[7] + ' | ' + board[8] + ' | ' + board[9])
+
+display_board ( ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'])
+
+
+
