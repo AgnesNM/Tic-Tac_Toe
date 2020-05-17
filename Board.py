07b9627d01939c71106656ed35f13c32cc188daa
Abby <nduta>
2020-05-17 10:21:03 +0300

    change order to mirror num order 7-8-9, 4-5-6, 1-2-3

--- Board.py
+++ Board.py
@@ -1,9 +1,6 @@
 def display_board(board):
-    print(board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print(board[4] + ' | ' + board[5] + ' | ' + board[6])
     print(board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(board[1] + ' | ' + board[2] + ' | ' + board[3])
 
 display_board ( ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'])
-
-
-
