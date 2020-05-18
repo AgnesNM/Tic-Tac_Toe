cc7485c2044eea937dde96d78178e9a153c5abd6
Abby <nduta>
2020-05-18 19:49:08 +0300

    allow user input to be output on the board. Need to allow for consecutive collection and display of user input as output on the board (some loop - for/while). Also find a way to refactor the code to display the initial empty board

--- Board.py
+++ Board.py
@@ -1,20 +1,19 @@
 user_input = input(" Do you choose X or O? ")
+input_position = input(" Where would you like to place it? Enter a number between 1 and 9: ")
+
 def show_options():
     if user_input == 'x'.lower():
         print(user_input)
+    if int(input_position) in range(1, 10):
+        print(input_position)
 show_options()
 
 def display_board(board):
-    for n,z in enumerate(board):
-        board[n] += user_input
-    print((' | ' + board[n] + ' | ')*3, "\n")
-    print((' | ' + board[n] + ' | ') * 3, "\n")
-    print((' | ' + board[n] + ' | ') * 3, "\n")
-    #print(' | ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + ' | ')
-    #print(' | ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + ' | ')
-    #print(' | ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' | ')
+    board[int(input_position)] += user_input
+    print(board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(board[1] + ' | ' + board[2] + ' | ' + board[3])
+
 display_board ([' '] *10)
 
-
-#collecting user input
 
