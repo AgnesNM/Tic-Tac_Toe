d3dee089bf6ebe921ba34e17e5e9ce8a2a31519e
Abby <nduta>
2020-05-19 20:22:09 +0300

    show user input for 2 consecutive rounds. Need to separate players, and map the numbers onto the display board

--- Board.py
+++ Board.py
@@ -1,3 +1,10 @@
+def display_board(board):
+    print(board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(board[1] + ' | ' + board[2] + ' | ' + board[3])
+
+display_board ([' '] *10)
+
 def repeat ():
     n = 0 #player chances to place an x or o
     global lst_user_input
@@ -14,21 +21,25 @@ def repeat ():
         break
 repeat()
 
-def display_board(board):
-    for index, num in enumerate(board):
+def updated_board(board):
+    for index in range(len(board)):
         if index == int(input_position):
-            for z in lst_user_input:
-                board[int(input_position)] += lst_user_input[0]
+            board[int(input_position)] += user_input
     print(board[7] + ' | ' + board[8] + ' | ' + board[9])
     print(board[4] + ' | ' + board[5] + ' | ' + board[6])
     print(board[1] + ' | ' + board[2] + ' | ' + board[3])
+    return board
 
-display_board ([' '] *10)
+newest = updated_board([' '] *10)
 
-
-#we need to call the updated board
 repeat()
-display_board ([' '] *10)
 
-
-
+def latest_board(newest):
+    for var in range(len(newest)):
+        if var == int(input_position):
+            newest[int(input_position)] += user_input
+            print(newest[7] + ' | ' + newest[8] + ' | ' + newest[9])
+            print(newest[4] + ' | ' + newest[5] + ' | ' + newest[6])
+            print(newest[1] + ' | ' + newest[2] + ' | ' + newest[3])
+    return newest
+latest_board(newest)
