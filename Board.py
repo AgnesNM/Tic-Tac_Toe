080da8cd64d7dbc62ff935a3b5b6ca0256947082
Abby <nduta>
2020-05-25 19:24:47 +0300

    add edgee cases for determining a winner

--- Board.py
+++ Board.py
@@ -1,11 +1,14 @@
 user_input_lst = []
 user_pos_lst = [ ]
+
+
 def display_board(board):
     print(board[7] + ' | ' + board[8] + ' | ' + board[9])
     print(board[4] + ' | ' + board[5] + ' | ' + board[6])
     print(board[1] + ' | ' + board[2] + ' | ' + board[3])
 
 display_board ([' '] *10)
+
 
 def repeat ():    
     global user_input
@@ -15,26 +18,53 @@ def repeat ():
     
 repeat()
 
+
 def collect_input():    
     user_input_lst.append(user_input)
     user_pos_lst.append(int(input_position))    
+
 collect_input()
+
+def combos_horizontal(lst):    
+    if user_pos_lst in lst:
+        print(f"player 1 wins!")     
+
+def combos_vertical(ver_lst):    
+    if user_pos_lst in ver_lst:
+        print(f"player 1 wins!")   
+
+def combos_diagonal(diag_lst):
+    print(f"These are the positions {user_pos_lst}")
+    if user_pos_lst in diag_lst:
+        print(f"player 1 wins!")
+    
 
 def winner():
     '''DECLARE A WINNER IF USER ENTERS 3 CONSECUTIVE X's OR O's'''
+    global code_x
     code_x = ['x','x','x']
     code_o = ['o','o','o']
     for user in range(len(user_input_lst)):
-        if user_input_lst == code_x:
+        if user_input_lst == code_x:            
             print(f"You entered 3 {user_input_lst[user]}'s! You win")
+
         elif user_input_lst == code_o:
            print(f"You entered 3 {user_input_lst[user]}'s! You win")
+
+        '''HORIZONTAL INDEX POSITION COMBINATIONS FOR CHECKING FOR WINNER'''   
+
+        combos_horizontal([[9,8,7], [6,5,4], [3,2,1], [7,8,9], [4,5,6], [1,2,3]])
+
+        combos_vertical([[1,4,7], [2,5,8], [3,6,9], [7,4,1], [8,5,2], [3,6,9]])
+        
+        combos_diagonal([[9,5,1], [7,5,3], [1,5,9], [3,5,7]])
         break    
 
-winner()
+winner()   
 
 
 def updated_board(board):
+    '''PRINT THE UPDATED BOARD'''
     for index in range(len(board)):
         if index == int(input_position):
             board[int(input_position)] += user_input
@@ -47,7 +77,7 @@ newest = updated_board([' '] *10)
 
 def rounds():
     r = 8
-    '''PRINT THE UPDATED BOARD'''
+    '''PRINT THE LATEST BOARD'''
     while r>0:
         repeat()    
         collect_input()
@@ -59,13 +89,13 @@ def rounds():
                     print(newest[7] + ' | ' + newest[8] + ' | ' + newest[9])
                     print(newest[4] + ' | ' + newest[5] + ' | ' + newest[6])
                     print(newest[1] + ' | ' + newest[2] + ' | ' + newest[3])
-            return newest
-
+            return newest 
+            
         latest_board(newest)
         r-=1
 
         if not winner() and r<=6:
-            break      
+            break         
 
 rounds()
 
