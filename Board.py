8d0520655465a18bf650752394d49a2379b96584
Abby <nduta>
2020-05-29 13:42:01 +0300

    declare winner if all  values in a row match

--- Board.py
+++ Board.py
@@ -1,12 +1,4 @@
-'''GLOBAL VARIABLES'''
-
-ongoing_game = True
-
-player_1_input_lst = []
-player_2_input_lst = []
-player_1_pos_lst = [ ]
-player_2_pos_lst = [ ]
-
+print("Welcome to Tic Tac Toe")
 
 def display_board(board):
     print(board[7] + ' | ' + board[8] + ' | ' + board[9])
@@ -14,6 +6,11 @@ def display_board(board):
     print(board[1] + ' | ' + board[2] + ' | ' + board[3])
 
 display_board ([' '] *10)
+
+'''GLOBAL VARIABLES'''
+
+ongoing_game = True
+
 
 def choose_players():
     global player
@@ -35,16 +32,6 @@ def repeat ():
 repeat()
 
 
-def collect_input():  
-    if player == "player 1":
-        player_1_input_lst.append(user_input)
-        player_1_pos_lst.append(int(input_position))        
-    elif player == "player2":
-        player_2_input_lst.append(user_input)
-        player_2_pos_lst.append(int(input_position))    
-
-collect_input()
-
 def updated_board(board):
     '''PRINT THE UPDATED BOARD'''
     for index in range(len(board)):
@@ -61,24 +48,26 @@ newest = updated_board([' '] *10)
 def rounds():
     
     '''PRINT THE LATEST BOARD'''
+    global ongoing_game
+
     while ongoing_game:
         repeat()    
-        collect_input()
-
+        
         def latest_board(newest):
             for var in range(len(newest)):
                 if var == int(input_position):
                     newest[int(input_position)] += user_input
-
-                    #print(f"this is {newest.index(newest[int(input_position)])}")
-                    
+                                       
                     print(newest[7] + ' | ' + newest[8] + ' | ' + newest[9])
                     print(newest[4] + ' | ' + newest[5] + ' | ' + newest[6])
-                    print(newest[1] + ' | ' + newest[2] + ' | ' + newest[3])                        
-                                      
-            return newest 
-                      
-        latest_board(newest)              
-        
-rounds()
+                    print(newest[1] + ' | ' + newest[2] + ' | ' + newest[3])   
 
+                if (newest[7] == newest[8] == newest[9] != ' ') :
+                    print(f"{player} wins")
+                    
+                    global ongoing_game
+                    ongoing_game = False     
+  
+        latest_board(newest)       
+
+rounds()
