1bd3158e4b9f1c7619d208c259bfd1addd33fefa
Nduta Red <agnesndutamwaniki@gmail.com>
2020-08-17 06:49:49 +0300

    write tests to check that user input is 'x' or 'o'

--- test_board.py
+++ test_board.py
@@ -4,7 +4,8 @@ import Board
 
 class TestBoard(unittest.TestCase):
     def test_global(self):
-        self.assertIn(Board.input_position, range(0,9))
+        #self.assertIn(Board.input_position, range(0,9)) #this test only checks the user's last input
+        self.assertEqual(Board.user_input, 'x' or 'o') #this test only checks the user's last input
 
 if __name__ == "__main__":
     unittest.main()
