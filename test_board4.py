91b28c95171eb456ab7ad717a6ce970ea212bc57
Nduta Red <agnesndutamwaniki@gmail.com>
2020-09-08 10:44:09 +0300

    write tests for the display-board function to check whether the printed board is using the input from the board which is the input

--- test_board4.py
+++ test_board4.py
@@ -9,6 +9,15 @@ class TestGame(unittest.TestCase):
     
     def test_global(self):             
         self.assertEqual(self.game1.player1, "x")
+        self.assertEqual(self.game1.player2, "o")
 
-if __name__ == "__main__":
+    def test_display_board(self):
+        mylist = ['x','1','2','3','4','5','6','7','8','9']
+        for fig in mylist:
+            with self.subTest(fig = fig):                
+                self.assertEqual(fig, fig)
+                
+               
+
+if __name__ == "__main__" :
     unittest.main()
