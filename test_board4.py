5eac4b80669e61d8c4f34d416367ffa50d2dad79
Nduta Red <agnesndutamwaniki@gmail.com>
2020-09-08 09:42:45 +0300

    write test to check whether player 1 is equal to 'x'

--- test_board4.py
+++ test_board4.py
@@ -5,10 +5,10 @@ from board4 import Game
 class TestGame(unittest.TestCase):
 
     def setUp(self):
-        self.game1 = Game("x", "o")
-
-    def test_clear_board(self):        
-        self.assertTrue(self.game1.clear_board)
+        self.game1 = Game("x", "o") 
+    
+    def test_global(self):             
+        self.assertEqual(self.game1.player1, "x")
 
 if __name__ == "__main__":
     unittest.main()
